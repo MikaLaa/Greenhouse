26796e7331757fd473684aee238bd2731b283930
Mika Laakso <52917639+MikaLaa@users.noreply.github.com>
2021-04-28 00:16:18 +0300

    RTOS added

--- Greenhouse.ino
+++ Greenhouse.ino
@@ -1,5 +1,6 @@
 #include <Wire.h> 
 #include <LiquidCrystal_I2C.h>
+#include <Arduino_FreeRTOS.h>
 
 #define SENS_1 A0
 #define SENS_2 A1
@@ -12,48 +13,87 @@ LiquidCrystal_I2C lcd(0x27,20,4);  // set the LCD address to 0x27
 unsigned int result_1;
 unsigned int result_2;
 
-void setup() {
-  pinMode(motorAPin_A, OUTPUT); //direction
-  pinMode(motorAPin_B, OUTPUT); //speed
-  pinMode(motorBPin_A, OUTPUT); //direction
-  pinMode(motorBPin_B, OUTPUT); //speed
-  delay(500);
+void TaskUpdateHumidity( void *pvParameters );
+
+void setup() 
+{
+ 
+
+
+   xTaskCreate(
+    TaskUpdateHumidity
+    ,  "TaskUpdateHumidity"  // A name just for humans
+    ,  128  // This stack size can be checked & adjusted by reading the Stack Highwater
+    ,  NULL
+    ,  1  // Priority, with 3 (configMAX_PRIORITIES - 1) being the highest, and 0 being the lowest.
+    ,  NULL );
+
+     xTaskCreate(
+    RunMotors
+    ,  "RunMotors"  // A name just for humans
+    ,  128  // This stack size can be checked & adjusted by reading the Stack Highwater
+    ,  NULL
+    ,  1  // Priority, with 3 (configMAX_PRIORITIES - 1) being the highest, and 0 being the lowest.
+    ,  NULL );
+
+}
+
+
+
+
+
+void loop() {
+  
+  delay(60000);// Odota minuutti
+}
+
+
+void TaskUpdateHumidity( void *pvParameters __attribute__((unused)) )  // This is a Task.
+{
   // initialize the lcd 
   lcd.init();               
   lcd.backlight();
   lcd.setCursor(1,0);
-  lcd.print("Starting");
-  delay(2000);
-}
 
-void loop() {
-  update_humidity();
-  if(result_1 > 300 || result_2 > 300)
+  for (;;) // A Task shall never return or exit.
   {
-    analogWrite(motorAPin_B, 127);// start pumb
-    analogWrite(motorBPin_B, 127);// start pumb
-    unsigned long startMillis = millis();
-    while(millis() - startMillis < 3000 )
-    {
-      // do nothing
-    }
-    analogWrite(motorAPin_B, 0);// stop pumb
-    analogWrite(motorBPin_B, 0);// stop pumb
+    result_1 = analogRead(SENS_1);
+    result_2 = analogRead(SENS_2);
+    lcd.setCursor(1,0);
+    lcd.print("SENSOR_1");
+    lcd.setCursor(10,0);
+    lcd.print(result_1);
+
+    lcd.setCursor(1,1);
+    lcd.print("SENSOR_2");
+    lcd.setCursor(10,1);
+    lcd.print(result_2);
   }
-  delay(60000);// Odota minuutti
+
+    vTaskDelay(2000);  // one tick delay (15ms) in between reads for stability 30s
 }
 
-void update_humidity()
+void RunMotors( void *pvParameters __attribute__((unused)) )  // This is a Task.
 {
-  result_1 = analogRead(SENS_1);
-  result_2 = analogRead(SENS_2);
-  lcd.setCursor(1,0);
-  lcd.print("SENSOR_1");
-  lcd.setCursor(10,0);
-  lcd.print(result_1);
+  pinMode(motorAPin_A, OUTPUT); //direction
+  pinMode(motorAPin_B, OUTPUT); //speed
+  pinMode(motorBPin_A, OUTPUT); //direction
+  pinMode(motorBPin_B, OUTPUT); //speed
 
-  lcd.setCursor(1,1);
-  lcd.print("SENSOR_2");
-  lcd.setCursor(10,1);
-  lcd.print(result_2);
+  for (;;) // A Task shall never return or exit.
+  {
+    if(result_1 > 300 || result_2 > 300)
+    {
+      analogWrite(motorAPin_B, 127);// start pumb
+      analogWrite(motorBPin_B, 127);// start pumb
+      unsigned long startMillis = millis();
+      while(millis() - startMillis < 3000 )
+      {
+        // do nothing this needs to be re thinked
+      }
+      analogWrite(motorAPin_B, 0);// stop pumb
+      analogWrite(motorBPin_B, 0);// stop pumb
+    }
+  }
+  vTaskDelay(4000);  // one tick delay (15ms) in between reads for stability 60s
 }
